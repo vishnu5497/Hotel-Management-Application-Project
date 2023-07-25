ba8e666452023f148a4b7974450842477bc8ae05
vishnu5497 <131842800+vishnu5497@users.noreply.github.com>
2023-07-25 14:53:13 +0530

    Delete day5/HotelManagement directory

--- day5/HotelManagement/HotelManagement/src/main/java/com/example/demo/Repositroy/Hotel.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repositroy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.HotelManagement;
-
-public interface Hotel extends JpaRepository<HotelManagement, Integer> {
-
-}
